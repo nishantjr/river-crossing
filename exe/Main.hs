c88fdb826f698c41509ed77dfda68ab3da266adf
Nishant <njr@cubic>
2026-05-13 18:26:59 +0530

    Extract configuration element for handling events
    We split event handling into two functions--one for dealing with window management
    (trivially implemented) and one for caching state.

--- exe/Main.hs
+++ exe/Main.hs
@@ -4,49 +4,78 @@ import           WXYZ.Protocol
 
 import           Control.Monad (void)
 import           Control.Monad.State
+import           Control.Monad.Extra (fromMaybeM)
+import           Control.Monad.Trans.Maybe (runMaybeT)
+
 
 data Window = Window RiverWindow RiverNode
 
 -- Cache of River's Window Management state
 data RiverState = RiverState { windows :: [Window] }
 
+-- immutable configuration.
+data WXYZConfig = WXYZConfig {
+        handleRiverEvent :: Event -> WXYZ (Maybe [Request])
+    }
+
 type WXYZ a = StateT RiverState IO a
 
-main :: IO ()
-main = do Just display <- wlDisplayConnect -- TODO: This should print an
+
+runWXYZ :: WXYZConfig -> IO ()
+runWXYZ config
+    = do Just display <- wlDisplayConnect -- TODO: This should print an
                                           -- error message.
-          initEventQueue
-          putStrLn "Connected to the Display."
-          True <- awaitRegistry display -- This prints a log message internally,
-                                        -- and isn't likely to happen except for
-                                        -- defective window managers. So it is
-                                        -- OK to rely on matching failure.
-          riverWM <- getRiverWM
-          riverWMAddEventListeners riverWM
-          void $ runStateT (eventLoop display) (RiverState [])
+         initEventQueue
+         putStrLn "Connected to the Display."
+         True <- awaitRegistry display -- This prints a log message internally,
+                                       -- and isn't likely to happen except for
+                                       -- defective window managers. So it is
+                                       -- OK to rely on matching failure.
+         riverWM <- getRiverWM
+         riverWMAddEventListeners riverWM
+         void $ runStateT (eventLoop display) (RiverState [])
+  where
+    eventLoop :: WlDisplay -> WXYZ ()
+    eventLoop display =
+        do e <- liftIO $ next_event display
+           case e of
+             Nothing -> pure ()
+             Just e' -> do requests <- fromMaybeM (unhandledEvent e')
+                                                  (handleRiverEvent config e')
+                           _ <- liftIO $ mapM (sendRequest display) requests
+                           eventLoop display
+    unhandledEvent e
+        = do liftIO $ putStrLn $ "unhandled event: " ++ (show e)
+             pure []
 
-eventLoop :: WlDisplay -> WXYZ ()
-eventLoop display =
-    do e <- liftIO $ next_event display
-       case e of
-         Nothing -> pure ()
-         Just e' -> do requests <- handleEvent e'
-                       _ <- liftIO $ mapM (sendRequest display) requests
-                       eventLoop display
 
-handleEvent :: Event -> WXYZ [Request]
-handleEvent (WMManageStart wm)
-    = pure [(WMManageFinish wm)]
-handleEvent (WMRenderStart wm) = pure [(WMRenderFinish wm)]
+(<||>) ::    (Event -> WXYZ (Maybe [Request]))
+          -> (Event -> WXYZ (Maybe [Request]))
+          ->  Event -> WXYZ (Maybe [Request])
+(<||>) h1 h2 e = do r1 <- h1 e
+                    case r1 of Nothing  -> h2 e
+                               Just _   -> pure r1
 
-handleEvent (WMWindow _wm win)
-    = do st <- get
-         let node = riverWindowGetNode win
-         put (RiverState $ (windows st) ++ [Window win $ riverWindowGetNode win])
-         pure [ (NodeSetPosition node 0 0)
-              , (WindowProposeDimensions win 0 0)
-              ]
+---
 
-handleEvent e
-    = do liftIO $ putStrLn $ "unhandled event: " ++ (show e)
-         pure []
+manageAndRender :: Event -> WXYZ (Maybe [Request])
+manageAndRender (WMManageStart wm) = runMaybeT $ pure [(WMManageFinish wm)]
+manageAndRender (WMRenderStart wm) = runMaybeT $ pure [(WMRenderFinish wm)]
+manageAndRender _ = pure Nothing
+
+cacheRiverState :: Event -> WXYZ (Maybe [Request])
+cacheRiverState (WMWindow _wm win) = runMaybeT $
+    do st <- get
+       let node = riverWindowGetNode win
+       put (RiverState $ (windows st) ++ [Window win $ riverWindowGetNode win])
+       pure [ (NodeSetPosition node 0 0)
+            , (WindowProposeDimensions win 0 0)
+            ]
+cacheRiverState _ = pure Nothing
+
+---
+
+main :: IO ()
+main = runWXYZ (WXYZConfig (cacheRiverState <||> manageAndRender))
+
+
