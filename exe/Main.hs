3282469ccd8dc8ba757aa98b8cc55ec78f9bf393
Nishant <njr@cubic>
2026-05-13 19:32:01 +0530

    Keep track of Windows in a Map instead of a List
    This lets us use a separate type for the window identifier other state
    such as minimized/fullscreen state

--- exe/Main.hs
+++ exe/Main.hs
@@ -6,12 +6,16 @@ import           Control.Monad (void)
 import           Control.Monad.State
 import           Control.Monad.Extra (fromMaybeM)
 import           Control.Monad.Trans.Maybe (runMaybeT)
+import           Data.Map (Map)
+import qualified Data.Map as M
 
 
-data Window = Window RiverWindow RiverNode
+data Window = Window { window :: RiverWindow
+                     , node   :: RiverNode
+                     }
 
 -- Cache of River's Window Management state
-data RiverState = RiverState { windows :: [Window] }
+data RiverState = RiverState { windows :: Map RiverWindow Window }
 
 -- immutable configuration.
 data WXYZConfig = WXYZConfig {
@@ -33,7 +37,7 @@ runWXYZ config
                                        -- OK to rely on matching failure.
          riverWM <- getRiverWM
          riverWMAddEventListeners riverWM
-         void $ runStateT (eventLoop display) (RiverState [])
+         void $ runStateT (eventLoop display) (RiverState M.empty)
   where
     eventLoop :: WlDisplay -> WXYZ ()
     eventLoop display =
@@ -43,7 +47,10 @@ runWXYZ config
              Just e' -> do requests <- fromMaybeM (unhandledEvent e')
                                                   (handleRiverEvent config e')
                            _ <- liftIO $ mapM (sendRequest display) requests
+                           st <- get
+                           liftIO $ putStrLn (show $ length $ windows st)
                            eventLoop display
+
     unhandledEvent e
         = do liftIO $ putStrLn $ "unhandled event: " ++ (show e)
              pure []
@@ -67,10 +74,14 @@ cacheRiverState :: Event -> WXYZ (Maybe [Request])
 cacheRiverState (WMWindow _wm win) = runMaybeT $
     do st <- get
        let node = riverWindowGetNode win
-       put (RiverState $ (windows st) ++ [Window win $ riverWindowGetNode win])
+       put $ st { windows = M.insert win (Window win node) (windows st) }
        pure [ (NodeSetPosition node 0 0)
             , (WindowProposeDimensions win 0 0)
             ]
+cacheRiverState (WindowClosed win) = runMaybeT $
+    do st <- get
+       put $ st { windows = M.delete win (windows st) }
+       pure []
 cacheRiverState _ = pure Nothing
 
 ---
